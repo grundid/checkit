b776a81a97c13ad371b75f0806422529479d78a7
webgapps.org <labs@webgapps.org>
2011-03-13 04:01:59 -0300

    version 1.1.1 released

---
+++ firefox/chrome/content/firstrun.js
@@ -0,0 +1,77 @@
+var checkItFirstrun = {
+
+		init: function(){//get current version from extension manager
+
+			try {// Firefox <= 3.6
+
+				//get current version from extension manager
+				var gExtensionManager = Components.classes["@mozilla.org/extensions/manager;1"]
+				.getService(Components.interfaces.nsIExtensionManager);
+				var current = gExtensionManager.getItemForID("checkit@lovinglinux.megabyet.net").version;
+
+				checkItFirstrun.updateInstall(current);
+			}
+			catch(e){// Firefox >=4.0
+
+				//get current version from extension manager
+				Components.utils.import("resource://gre/modules/AddonManager.jsm");
+
+				AddonManager.getAddonByID("checkit@lovinglinux.megabyet.net", function(addon) {
+
+					var current = addon.version;
+					checkItFirstrun.updateInstall(current);
+				});
+			}
+			window.removeEventListener("load",function(){ flashaidFirstrun.init(); },true);
+		},
+
+		updateInstall: function(aVersion){//check version and perform updates
+			
+			//access preferences interface
+			this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+			.getService(Components.interfaces.nsIPrefService)
+			.getBranch("extensions.checkit.");
+
+			//firstrun, update and current declarations
+			var ver = -1, firstrun = true;
+			var current = aVersion;
+
+			try{//check for existing preferences
+				ver = this.prefs.getCharPref("version");
+				firstrun = this.prefs.getBoolPref("firstrun");
+			}catch(e){
+				//nothing
+			}finally{
+
+				if(firstrun){//actions specific for first installation
+
+					var navbar = document.getElementById("nav-bar");
+					var newset = navbar.currentSet + ",checkit-toolbar-button";
+					navbar.currentSet = newset;
+					navbar.setAttribute("currentset", newset );
+					document.persist("nav-bar", "currentset");
+
+					//set preferences
+					this.prefs.setBoolPref("firstrun",false);
+					this.prefs.setCharPref("version",current);
+				}
+
+				if(ver !== current && !firstrun){//actions specific for extension updates
+
+					//set preferences
+					this.prefs.setCharPref("version",current);
+
+					if(ver !== "1.1.1"){
+
+						var navbar = document.getElementById("nav-bar");
+						var newset = navbar.currentSet + ",checkit-toolbar-button";
+						navbar.currentSet = newset;
+						navbar.setAttribute("currentset", newset );
+						document.persist("nav-bar", "currentset");
+					}
+				}
+			}
+		}
+};
+//event listeners to call the functions when Firefox starts
+window.addEventListener("load",function(){ checkItFirstrun.init(); },true);
--- firefox/chrome/content/interface.js
+++ firefox/chrome/content/interface.js
@@ -1,23 +1,125 @@
 var checkItInterface = {
-		compareChecksum: function () {
-			strbundle = document.getElementById("checkitstrings");
-			var originalchecksum = content.getSelection().toString().replace(/[^0-9a-f]/ig, '');
-			var aType='MD5';
-			switch(originalchecksum.length) {
-			case 40: var aType='SHA1'; break;
-			case 64: var aType='SHA256'; break;
-			case 96: var aType='SHA384'; break;
-			case 128: var aType='SHA512'; break;
+
+		init: function () {
+			var contextMenu = document.getElementById("checkit-contextmenu");
+			if (contextMenu) {
+				contextMenu.addEventListener("popupshowing", function(e) { checkItInterface.onShowing(e); }, false);
+				contextMenu.addEventListener("popuphiding", function(e) { checkItInterface.onHiding(e); }, false);
 			}
+		},
+
+		onShowing: function (e) {
+			var entry = document.getElementById("checkit-hash");
+			var orig = content.getSelection().toString().replace(/[^0-9a-f]/ig, '');
+			var type = checkItInterface.getChecksumType(orig);
+			if ( type ) {
+				entry.label = document.getElementById("checkitstrings").getString("selectedhash")+' ('+type+')';
+				entry.hidden = false;
+			}
+		},
+
+		onHiding: function (e) {
+			var entry = document.getElementById("checkit-hash");
+			entry.hidden = true;
+			entry.label = document.getElementById("checkitstrings").getString("checkitalert");
+		},
+
+		getChecksumType: function (checksum) {
+			switch(checksum.length) {
+			case 32: return 'MD5'; break;
+			case 40: return 'SHA1'; break;
+			case 64: return 'SHA256'; break;
+			case 96: return 'SHA384'; break;
+			case 128: return 'SHA512'; break;
+			default: return false;
+			}
+		},
+
+		compareChecksum: function () {
+
+			//get localization strings
+			var strbundle = document.getElementById("checkitstrings");
+			var originalchecksum, filechecksum, aType, message, messagetitle, prompts, alertsService;
+
+			//get selected hash and check type
+			originalchecksum = content.getSelection().toString().replace(/[^0-9a-f]/ig, '');
+			aType = checkItInterface.getChecksumType(originalchecksum);
+
+			//trigger invalidhash alert
+			if ( !aType ) {
+				message = strbundle.getString("invalidhash");
+				messagetitle = strbundle.getString("checkitalert");
+				prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+				.getService(Components.interfaces.nsIPromptService);
+				prompts.alert(window, messagetitle, message);
+				return false;
+			}
+
+			filechecksum = checkItInterface.getFileHash(aType);
+			originalchecksum = originalchecksum.toLowerCase();
+
+			//trigger alerts
+			if (filechecksum == originalchecksum){
+				message = aType+' '+strbundle.getString("isamatch");
+				messagetitle = strbundle.getString("checkitmessage");
+				var alertsService = Components.classes["@mozilla.org/alerts-service;1"]
+				.getService(Components.interfaces.nsIAlertsService);
+				alertsService.showAlertNotification("chrome://checkit/skin/icon48.png",
+						messagetitle, message,
+						false, "", null);
+			} else {
+				message = aType+' '+strbundle.getFormattedString("notamatch", [ originalchecksum, filechecksum ]);
+				messagetitle = strbundle.getString("checkitalert");
+				prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+				.getService(Components.interfaces.nsIPromptService);
+				prompts.alert(window, messagetitle, message);
+			}
+		},
+
+		compareFiles: function (aType) {
+
+			//get localization strings
+			var strbundle = document.getElementById("checkitstrings");
+			var file1checksum, file2checksum, message, messagetitle, prompts, alertsService;
+
+			file1checksum = checkItInterface.getFileHash(aType);
+			file2checksum = checkItInterface.getFileHash(aType);
+
+			if (file1checksum == file2checksum){
+				message = aType+' '+strbundle.getString("isamatch");
+				messagetitle = strbundle.getString("checkitmessage");
+				var alertsService = Components.classes["@mozilla.org/alerts-service;1"]
+				.getService(Components.interfaces.nsIAlertsService);
+				alertsService.showAlertNotification("chrome://checkit/skin/icon48.png",
+						messagetitle, message,
+						false, "", null);
+			} else {
+				message = aType+' '+strbundle.getFormattedString("filesnotmatch", [ file1checksum, file2checksum ]);
+				messagetitle = strbundle.getString("checkitalert");
+				prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+				.getService(Components.interfaces.nsIPromptService);
+				prompts.alert(window, messagetitle, message);
+			}
+		},
+
+		getFileHash: function (aType) {
+
+			//get localization strings
+			var strbundle = document.getElementById("checkitstrings");
+
+			//get file and generate hash
 			var nsIFilePicker = Components.interfaces.nsIFilePicker;
 			var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
 			fp.init(window, strbundle.getFormattedString("selectafile", [ aType ]), nsIFilePicker.modeOpen);
 			var rv = fp.show();
 			if (rv == nsIFilePicker.returnOK) {
 				var f = fp.file;
-				var istream = Components.classes["@mozilla.org/network/file-input-stream;1"]		   
+				//open input stream
+				var istream = Components.classes["@mozilla.org/network/file-input-stream;1"]
 				.createInstance(Components.interfaces.nsIFileInputStream);
 				istream.init(f, 0x01, 0444, 0);
+
+				//hash check
 				var ch = Components.classes["@mozilla.org/security/hash;1"]
 				.createInstance(Components.interfaces.nsICryptoHash);
 				ch.init(ch[aType]);
@@ -26,24 +128,11 @@ var checkItInterface = {
 				var hash = ch.finish(false);
 				function toHexString(charCode){ return ("0" + charCode.toString(16)).slice(-2); }
 				var filechecksum = [toHexString(hash.charCodeAt(i)) for (i in hash)].join("");
-				var filechecksum = filechecksum.replace(/[^0-9a-f]/ig, '');
-				var filechecksum = filechecksum.toLowerCase();
-				var originalchecksum = originalchecksum.toLowerCase();
-				if (filechecksum == originalchecksum){
-					var message = aType+' '+strbundle.getString("isamatch");
-					var messagetitle = strbundle.getString("checkitmessage");
-					var alertsService = Components.classes["@mozilla.org/alerts-service;1"]
-					.getService(Components.interfaces.nsIAlertsService);
-					alertsService.showAlertNotification("chrome://checkit/skin/icon32.png",
-							messagetitle, message,
-							false, "", null);
-				} else {
-					var message = aType+' '+strbundle.getFormattedString("notamatch", [ originalchecksum, filechecksum ]);
-					var messagetitle = strbundle.getString("checkitalert");
-					var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-					.getService(Components.interfaces.nsIPromptService);
-					prompts.alert(window, messagetitle, message);
-				}
+				filechecksum = filechecksum.replace(/[^0-9a-f]/ig, '');
+				filechecksum = filechecksum.toLowerCase();
+				return filechecksum;
 			}
 		}
 };
+
+window.addEventListener("load", checkItInterface.init, false);
